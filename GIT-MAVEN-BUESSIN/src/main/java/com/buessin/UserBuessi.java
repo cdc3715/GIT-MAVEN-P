27242a19be0162c2821a9498d76c3d8825bbbd88
cdc3715 <464278398@qq.com>
2017-01-06 16:23:48 +0800

    初始还 P

---
+++ GIT-MAVEN-BUESSIN/src/main/java/com/buessin/UserBuessi.java
@@ -0,0 +1,11 @@
+package com.buessin;
+
+import com.model.User;
+
+public class UserBuessi {
+	public static void main(String[] args) {
+		User user =new User();
+		user.setName("HelloWord");
+		System.out.println(user.getName());
+	}
+}
